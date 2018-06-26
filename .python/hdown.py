2e5c0c326c5d6349d65b38071c0eaa0b36ff5198
Hau5ratz <16449790+Hau5ratz@users.noreply.github.com>
2018-06-26 10:59:09 -0400

    Create hdown.py

---
+++ .python/hdown.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+import sys
+import os
+import shutil
+
+def download_file(url, x):
+    local_filename = str(x) +'.jpg'
+    # NOTE the stream=True parameter
+    r = requests.get(url, stream=True)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024): 
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+                #f.flush() commented by recommendation from J.F.Sebastian
+    return local_filename
+
+Base = 'https://i.nhentai.net/galleries/%s/%s.jpg'
+
+url = sys.argv[1] if len(sys.argv) >= 2 else Sample
+
+
+r = requests.get(url)
+soup = BeautifulSoup(r.content, 'html.parser')
+Imgs = soup.find_all('img')
+idy = [x['src'] for x in Imgs if 'jpg' in x['src'] and not 'thumb' in x['src']][-1].split('/')[-2]
+
+Pagenum = int(input('how many page numbers?: '))
+
+D = str(raw_input('Foldername?: '))
+os.makedirs(D)
+os.chdir(D)
+for x in range(1, Pagenum+1):
+    F = Base%(idy, x)
+    download_file(F, x)
+    print 'Downloaded %s out of %s'%(x, Pagenum)
+os.chdir('..')
+shutil.move(D,'../Pictures')
+  
+ 
