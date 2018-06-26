98ec3b3ae804ec7ed16fb5d8d942490d6f783b09
Hau5ratz <16449790+Hau5ratz@users.noreply.github.com>
2018-06-26 14:35:01 -0400

    Update downbot.py

--- .python/downbot.py
+++ .python/downbot.py
@@ -4,37 +4,62 @@ import sys
 import os
 import shutil
 
-def download_file(url, x):
-    local_filename = str(x) + raw_input('Suffix?: ')
-    # NOTE the stream=True parameter
-    r = requests.get(url, stream=True)
-    with open(local_filename, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=1024): 
-            if chunk: # filter out keep-alive new chunks
-                f.write(chunk)
-    return local_filename
 
-Base = 'https://www.watchcartoononline.io/'
+class Downloader():
+    '''
+    Downloader utility class
+    if passed a url name pair it will download it
+    if passed a list of url name pair will download all of them
+    '''
 
-url = sys.argv[1] if len(sys.argv) >= 2 else Sample
+    def __init__(self, *args):
+        self.rurl = r'''^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$'''
+        self.rsuf = r'''^.*\.\w+$'''
+        self.test_pass(args)
 
+    def test_pass(self, obj):
+        if isinstance(obj, tuple):
+            self.download_file(*obj)
+        elif isinstance(obj, list):
+            for x in obj:
+                self.download_file(*x)
+        else:
+            assert False, 'Incorrect arguement formate'
 
-r = requests.get(url)
-soup = BeautifulSoup(r.content, 'html.parser')
-Imgs = soup.find_all('img')
-idy = [x['src'] for x in Imgs if 'jpg' in x['src'] and not 'thumb' in x['src']][-1].split('/')[-2]
+    def test(self, obj):
+        '''
+        Tests object to make sure it conforms to standard
+        '''
+        d = ''
+        if len(obj) == 3:
+            d = os.listdir()
+            if not obj[2] in d:
+                print 'directory does not exist, making now..'
+                os.mkdir(obj[2])
+                print 'directory made'
+                d = obj[2]
+            else:
+                d = obj[2]
+        assert all([isinstance(x, str)
+                    for x in args]), 'all arguements must be string'
+        assert re.match(
+            self.rurl, args[0]), '1st arguement must be valid URL'
+        assert re.match(
+            self.rsuf, args[1]), '2nd arguement must be a valid filename'
 
-Pagenum = int(input('how many page numbers?: '))
+    def download_file(self, url, x, d=''):
+        '''
+        Takes a file URL and name as arguement and downloads and writes it
+        '''
+        self.test(url, x, d)
+        local_filename = str(x)
+        # NOTE the stream=True parameter
+        r = requests.get(url, stream=True)
+        with open('/' + d + '/' + local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024):
+                if chunk:  # filter out keep-alive new chunks
+                    f.write(chunk)
+        return local_filename
 
-D = str(raw_input('Foldername?: '))
-os.makedirs(D)
-os.chdir(D)
-for x in range(1, Pagenum+1):
-    F = Base%(idy, x)
-    download_file(F, x)
-    print 'Downloaded %s out of %s'%(x, Pagenum)
-os.chdir('..')
-shutil.move(D,'../Pictures')
-  
- 
-
+if __name__ == '__main__':
+    Downloader(sys.argv[1:])
